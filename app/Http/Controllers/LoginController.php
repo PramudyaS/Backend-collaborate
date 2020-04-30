d40b05d37ca15855cb26b1aff7cce0d3d0804bc5
PramudyaS <pramudyasaputra89@gmail.com>
2020-04-30 21:12:56 +0700

    Login API

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Hash;
+use Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+    	$user = User::where('email',$request->credentials['email'])->first();
+    	if (!$user || !Hash::check($request->credentials['password'], $user->password)) {
+    		return response()->json(['message'=>'These credentials do not match our records'],404);
+    	}
+
+    	Auth::attempt(['email'=>$request->credentials['email'],'password'=>$request->credentials['password']]);
+
+    	$token = $user->createToken('collaborate-app')->plainTextToken;
+
+    	return response()->json(['user'=>Auth::user(),'token'=>$token],201);
+    }
+}

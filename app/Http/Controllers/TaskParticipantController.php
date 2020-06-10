0c5dcc21b90cb8142184f9160ccef6905433d0e5
PramudyaS <pramudyasaputra89@gmail.com>
2020-06-10 15:07:44 +0700

    add : DB Participant and team

---
+++ app/Http/Controllers/TaskParticipantController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TaskParticipant;
+use Illuminate\Http\Request;
+
+class TaskParticipantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TaskParticipant  $taskParticipant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TaskParticipant $taskParticipant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TaskParticipant  $taskParticipant
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TaskParticipant $taskParticipant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TaskParticipant  $taskParticipant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TaskParticipant $taskParticipant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TaskParticipant  $taskParticipant
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TaskParticipant $taskParticipant)
+    {
+        //
+    }
+}
---
+++ app/TaskParticipant.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TaskParticipant extends Model
+{
+    //
+}
